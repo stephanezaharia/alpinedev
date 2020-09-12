7b7e26c353908857572748e82086d0b9fbd66cdf
stephane zaharia <stephane.zaharia@gmail.com>
2020-09-12 16:52:24 +0000

    First commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build . -t devalpine -f Devalpine
+docker build . -t devtools -f Devchoucas
---
+++ dev.sh
@@ -0,0 +1,1 @@
+docker run -v /share:/share -v /share/homes:/home -v /var/run/docker.sock:/var/run/docker.sock -it devtools /bin/bash
